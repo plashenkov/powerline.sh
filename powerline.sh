2669292325c55cdc7d5bd34a9d4407f1c171fc0d
Yuri Plashenkov <plashenkov@gmail.com>
2019-04-15 04:19:17 +0300

    Fix mc

--- powerline.sh
+++ powerline.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
 # Beautiful powerline-style prompt written in pure bash, simple and very fast.
-# 
-# Author  : Yury Plashenkov
-# Website : https://yuryplashenkov.com/en/projects/powerline
-# Github  : https://github.com/plashenkov/powerline 
+#
+# Author  : Yuri Plashenkov
+# Github  : https://github.com/plashenkov/powerline
 # License : MIT License
 
 __ps1() {
@@ -19,35 +18,35 @@ __ps1() {
   local path_separator_color=245
   local path_bold=false
 
-  if [ "$host_bold" = true ]; then host_bold="1;"; else host_bold=""; fi
-  if [ "$path_bold" = true ]; then path_bold="1;"; else path_bold=""; fi
+  # Skip Midnight Commander
+  if [ -z $MC_SID ]; then
+    if [ "$host_bold" = true ]; then host_bold="1;"; else host_bold=""; fi
+    if [ "$path_bold" = true ]; then path_bold="1;"; else path_bold=""; fi
 
-  local path=$PWD
+    local path=$PWD
 
-  if [ "$path" != "/" ]; then
-    # Replace home directory with ~
-    path=${path/#$HOME/\~}
+    if [ "$path" != "/" ]; then
+      # Replace home directory with ~
+      path=${path/#$HOME/\~}
 
-    # Strip leading and trailing slashes
-    path=${path#/}
-    path=${path%/}
+      # Strip leading and trailing slashes
+      path=${path#/}
+      path=${path%/}
 
-    # Strip too long path
-    IFS="/" read -ra dirs <<< "$path"
-    local dir_count=${#dirs[@]}
+      # Strip too long path
+      IFS="/" read -ra dirs <<< "$path"
+      local dir_count=${#dirs[@]}
 
-    if [ $dir_count -gt 4 ]; then
-      path=".../${dirs[$dir_count-3]}/${dirs[$dir_count-2]}/${dirs[$dir_count-1]}"
+      if [ $dir_count -gt 4 ]; then
+        path=".../${dirs[$dir_count-3]}/${dirs[$dir_count-2]}/${dirs[$dir_count-1]}"
+      fi
+
+      # Replace path separator (/) with powerline separator symbol
+      path=${path//\//"\[\e[0;38;5;${path_separator_color};48;5;${path_bg_color}m\]  \[\e[${path_bold}38;5;${path_text_color}m\]"}
     fi
 
-    # Replace path separator (/) with powerline separator symbol
-    path=${path//\//"\[\e[0;38;5;${path_separator_color};48;5;${path_bg_color}m\]  \[\e[${path_bold}38;5;${path_text_color}m\]"}
+    PS1="\n\[\e[${host_bold}48;5;${host_bg_color};38;5;${host_text_color}m\] ${host} \[\e[0;38;5;${host_bg_color};48;5;${path_bg_color}m\]\[\e[${path_bold}48;5;${path_bg_color};38;5;${path_text_color}m\] $path \[\e[0;38;5;${path_bg_color}m\]\[\e[0m\] "
   fi
-
-  PS1="\n\[\e[${host_bold}48;5;${host_bg_color};38;5;${host_text_color}m\] ${host} \[\e[0;38;5;${host_bg_color};48;5;${path_bg_color}m\]\[\e[${path_bold}48;5;${path_bg_color};38;5;${path_text_color}m\] $path \[\e[0;38;5;${path_bg_color}m\]\[\e[0m\] "
 }
 
-# Skip Midnight Commander
-if ! ps $PPID | grep -q mc; then
-  PROMPT_COMMAND=__ps1
-fi
+PROMPT_COMMAND=__ps1
